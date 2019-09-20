ccc4011dfc180670fce6fe585a420b096421aea2
Alex <a_zobnin@mail.ru>
2019-09-20 21:18:58 +0300

    Исправления в Учебный проект: нас орда по замечаниям Наставника #1

--- js/setup.js
+++ js/setup.js
@@ -2,29 +2,28 @@
 
 var WIZARD_COUNT = 4;
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_FAMILYES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['101, 137, 164', '241, 43, 107', '146, 100, 161', '56, 159, 117', '215, 210, 55', '0, 0, 0'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var getRandomIndex = function (upperLimit) {
+  return Math.floor(Math.random() * upperLimit);
+};
 var getRandomWizardName = function (names) {
-  var index = Math.floor(Math.random() * names.length);
-  return names[index];
+  return names[getRandomIndex(names.length)];
 };
 
-var getRandomWizardFamily = function (familyes) {
-  var index = Math.floor(Math.random() * familyes.length);
-  return familyes[index];
+var getRandomWizardSurname = function (familyes) {
+  return familyes[getRandomIndex(familyes.length)];
 };
 
 var getRandomCoatColor = function (coatColors) {
-  var index = Math.floor(Math.random() * coatColors.length);
-  var color = coatColors[index];
+  var color = coatColors[getRandomIndex(coatColors.length)];
   return 'rgb(' + color + ')';
 };
 
 var getRandomEyesColor = function (eyeColors) {
-  var index = Math.floor(Math.random() * eyeColors.length);
-  return eyeColors[index];
+  return eyeColors[getRandomIndex(eyeColors.length)];
 };
 
 var renderWizard = function (wizard) {
@@ -37,14 +36,16 @@ var renderWizard = function (wizard) {
 
 var wizards = [];
 
-var createWizardData = function (count, wizArray) {
+var createWizardData = function (count) {
+  var wizArray = [];
   for (var i = 0; i < count; i++) {
     var wizard = {};
-    wizard.name = getRandomWizardName(WIZARD_NAMES) + ' ' + getRandomWizardFamily(WIZARD_FAMILYES);
+    wizard.name = getRandomWizardName(WIZARD_NAMES) + ' ' + getRandomWizardSurname(WIZARD_SURNAMES);
     wizard.eyeColor = getRandomEyesColor(EYES_COLORS);
     wizard.coatColor = getRandomCoatColor(COAT_COLORS);
     wizArray[i] = wizard;
   }
+  return wizArray;
 };
 
 var userSetup = document.querySelector('.setup');
@@ -53,7 +54,7 @@ var similarListElem = document.querySelector('.setup-similar-list');
 var similarWizardTempl = document.querySelector('#similar-wizard-template')
 .content
 .querySelector('.setup-similar-item');
-createWizardData(WIZARD_COUNT, wizards);
+wizards = createWizardData(WIZARD_COUNT);
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < WIZARD_COUNT; i++) {
