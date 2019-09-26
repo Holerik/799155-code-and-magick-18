5c6fb012d63a7f7ad64b5e7f6b6420ea421bb103
Alex <a_zobnin@mail.ru>
2019-09-26 21:49:01 +0300

    Учебный проект: одеть одежду

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,11 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['101, 137, 164', '241, 43, 107', '146, 100, 161', '56, 159, 117', '215, 210, 55', '0, 0, 0'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var DOM_VK_ENTER = 0x0D;
+var DOM_VK_ESC = 0x1B;
+// var MIN_NAME_LENGTH = 2;
+// var MAX_NAME_LENGTH = 25;
 
 var getRandomIndex = function (upperLimit) {
   return Math.floor(Math.random() * upperLimit);
@@ -24,6 +29,10 @@ var getRandomCoatColor = function (coatColors) {
 
 var getRandomEyesColor = function (eyeColors) {
   return eyeColors[getRandomIndex(eyeColors.length)];
+};
+
+var getRandomFireballColor = function (fireballColors) {
+  return fireballColors[getRandomIndex(fireballColors.length)];
 };
 
 var renderWizard = function (wizard) {
@@ -48,8 +57,8 @@ var createWizardData = function (count) {
   return wizArray;
 };
 
-var userSetup = document.querySelector('.setup');
-userSetup.classList.remove('hidden');
+var setup = document.querySelector('.setup');
+// userSetup.classList.remove('hidden');
 var similarListElem = document.querySelector('.setup-similar-list');
 var similarWizardTempl = document.querySelector('#similar-wizard-template')
 .content
@@ -61,4 +70,80 @@ for (var i = 0; i < WIZARD_COUNT; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElem.appendChild(fragment);
-document.querySelector('.setup-similar').classList.remove('hidden');
+// document.querySelector('.setup-similar').classList.remove('hidden');
+
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var userNameInput = setup.querySelector('.setup-user-name');
+var closeButton = setup.querySelector('.setup-submit');
+
+var onPopupPressEsc = function (evt) {
+  if (evt.keyCode === DOM_VK_ESC) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupPressEsc);
+};
+
+var closePopup = function () {
+  if (userNameInput === document.activeElement) {
+    return;
+  }
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupPressEsc);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === DOM_VK_ENTER) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === DOM_VK_ENTER) {
+    closePopup();
+  }
+});
+
+closeButton.addEventListener('click', function () {
+  closePopup();
+});
+
+// для ограничений на ввод используем стандартные возможности форм HTML5
+userNameInput.addEventListener('invalid', function (evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х букв');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Длина имени не должна превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = getRandomCoatColor(COAT_COLORS);
+});
+
+var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = getRandomEyesColor(EYES_COLORS);
+});
+
+var fireBall = document.querySelector('.setup-fireball-wrap');
+fireBall.addEventListener('click', function () {
+  fireBall.style.background = getRandomFireballColor(FIREBALL_COLORS);
+});
