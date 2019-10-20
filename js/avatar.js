fb689e68d9a6c47cd9adf56d124af74bc0979667
Alex <a_zobnin@mail.ru>
2019-10-20 16:35:32 +0300

    Учебный проект: выкладываем фото

---
+++ js/avatar.js
@@ -0,0 +1,24 @@
+// avatar.js
+'use strict';
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var fileChooser = document.querySelector('.upload input[type=file]');
+  var picture = document.querySelector('.setup-user-pic');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    if (file) {
+      var fileName = file.name.toLowerCase();
+      var matches = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
+      });
+      if (matches) {
+        var reader = new FileReader();
+        reader.addEventListener('load', function () {
+          picture.src = reader.result;
+        });
+        reader.readAsDataURL(file);
+      }
+    }
+  });
+})();
